bf5c4b12961c081f41f7bab6820c660162bcb2e8
AlexSpeal <alexspeal@gmail.com>
2025-02-25 14:44:10 +0300

    init commit

---
+++ src/main/java/alexspeal/meeting_site/MeetingSiteApplication.java
@@ -0,0 +1,13 @@
+package alexspeal.meeting_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexspeal/meeting_site/MeetingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package alexspeal.meeting_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
